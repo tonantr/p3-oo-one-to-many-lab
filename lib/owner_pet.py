e9859e79952ac3180bf143fb9636fc594bea8eb7
tonan <tonantruong@hotmail.com>
2023-07-31 23:52:36 -0400

    completed one-to-many Obj relationships lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,35 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        if pet_type not in Pet.PET_TYPES:
+            raise TypeError("Pet type is not correct")
+        else:
+            self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+    
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError('Pet must be an instance of Pet class')
+        pet.owner = self
+    
+    def get_sorted_pets(self):
+        return sorted(Pet.all, key=lambda x: x.name)
+
+
+
+
+
+
+
